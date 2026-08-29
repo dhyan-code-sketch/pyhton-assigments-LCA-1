0608ae814f0e4dc0332a96d20733d88f1ec0da29
DHYAN PATEL <dhyanpatel11785@gmail.com>
2026-08-29 17:22:16 +0530

    Add files via upload

---
+++ assigment 2.py
@@ -0,0 +1,14 @@
+##### code to find greatest of three numbers ####
+a = int(input("enter your number 1:"))
+
+b = int(input("enter your number 2:"))
+c = int(input("enter your number 3:"))
+
+if a > b and a > c:
+    print("a is the greatest")
+
+elif a < b and b > c:
+    print("b is the greatest")
+
+elif c > a and c > b:
+    print("c is the greatest")
