dce0f86483ae25110d46c62afeddac9a15ec05a4
DHYAN PATEL <dhyanpatel11785@gmail.com>
2026-09-14 18:52:31 +0530

    Add files via upload

---
+++ assigment 5.py
@@ -0,0 +1,10 @@
+                ###by using isalnum() method ###
+
+timepass=input("enter your string")
+    #isalnum functions check if the given string has alphabet or numerical value#
+if timepass.isalnum():
+    print("the timepass has only A-Z,a-z,0-9")
+else:
+    print("the timepass contains other characters")
+
+                    ### THANK YOU ###
